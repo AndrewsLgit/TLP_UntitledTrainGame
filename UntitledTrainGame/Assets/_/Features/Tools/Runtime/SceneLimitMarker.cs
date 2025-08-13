fa9ebd9ea0d3b2b0c7c3906f97f75a1507008158
AndrewsLgit <andrewsleruth@gmail.com>
2025-08-13 15:32:52 +0200

    Added Limit markers for camera player follow

---
+++ UntitledTrainGame/Assets/_/Features/Tools/Runtime/SceneLimitMarker.cs
@@ -0,0 +1,8 @@
+using Foundation.Runtime;
+using UnityEngine;
+
+namespace Tools.Runtime
+{
+    // empty script used as marker to identify map limits
+    public class SceneLimitMarker : FMono{ }
+}
