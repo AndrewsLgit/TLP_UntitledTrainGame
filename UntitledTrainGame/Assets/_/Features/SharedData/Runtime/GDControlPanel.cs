c00d082b5ed2077034abad65cbdc4410011fe580
AndrewsLgit <andrewsleruth@gmail.com>
2025-08-20 10:16:19 +0200

    added time compression variable

--- UntitledTrainGame/Assets/_/Features/SharedData/Runtime/GDControlPanel.cs
+++ UntitledTrainGame/Assets/_/Features/SharedData/Runtime/GDControlPanel.cs
@@ -12,12 +12,12 @@ namespace SharedData.Runtime
         
         [Header("Player Movement")]
         [SerializeField] private float _playerMoveSpeed = 3f;
-        
         [SerializeField, Range(0,1)] private float _turnSmoothTime = .1f;
         
         [Header("Camera Settings")]
         [SerializeField, Range(0,1f)] private float _edgeThresholdY = 0.1f; 
         [SerializeField, Range(0,1f)] private float _edgeThresholdX = 0.1f;
+        
         [Header("Camera Rotation Settings")]
         [SerializeField, Range(0,10f)] private float _markerDistanceThreshold = 5f;
         [SerializeField, Range(0,45f)] private float _maxRotationAngle = 10f;
@@ -27,10 +27,12 @@ namespace SharedData.Runtime
             new Keyframe(0, 0),
             new Keyframe(1, 1)
             );
-
         [SerializeField] private AnimationCurve _cameraReturnCurve = new AnimationCurve(
             new Keyframe(0,1),
             new Keyframe(1,0));
+        
+        [Header("Time System")]
+        [SerializeField, Range(0,1)] private float _compressionFactor = 0.5f;
         
         // Private Variables
         #endregion
@@ -41,14 +43,18 @@ namespace SharedData.Runtime
         
         public float PlayerMoveSpeed => _playerMoveSpeed;
         public float TurnSmoothTime => _turnSmoothTime;
+        
         public float EdgeThresholdY => _edgeThresholdY;
         public float EdgeThresholdX => _edgeThresholdX;
+        
         public float MarkerDistanceThreshold => _markerDistanceThreshold;
         public float MaxRotationAngle => _maxRotationAngle;
         public float RotationSpeed => _rotationSpeed;
         public float SmoothTime => _smoothTime;
         public AnimationCurve CameraRotationCurve => _cameraRotationCurve;
         public AnimationCurve CameraReturnCurve => _cameraReturnCurve;
+        
+        public float CompressionFactor => _compressionFactor;
         // Public Variables
         #endregion
         
