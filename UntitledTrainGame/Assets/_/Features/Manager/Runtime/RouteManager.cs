eec45cc254a8e4d5e1ff405a82b46493ccb5f4f6
AndrewsLgit <andrewsleruth@gmail.com>
2025-08-20 10:16:57 +0200

    added Additive scene preloading and unloading of previous scene

--- UntitledTrainGame/Assets/_/Features/Manager/Runtime/RouteManager.cs
+++ UntitledTrainGame/Assets/_/Features/Manager/Runtime/RouteManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using Foundation.Runtime;
 using SharedData.Runtime;
+using Tools.Runtime;
 using UnityEngine;
 
 namespace Manager.Runtime
@@ -12,28 +14,59 @@ namespace Manager.Runtime
         #region Private
         // Private Variables
         
+        // References
         [SerializeField] private StationNetwork_Data _stationNetwork;
         [SerializeField] private TrainRoute_Data _testTrainRoute;
+        [SerializeField] private GDControlPanel _controlPanel;
+        private SceneLoader _sceneLoader;
 
         private List<Station_Data> _segments = new List<Station_Data>();
         private int _currentStationIndex = 0;
+        private float _compressionFactor = 0.05f;
 
         // Private Variables
         #endregion
-
+        
+        #region Public
+        // Public Variables
+        
+        public static RouteManager Instance { get; private set; }
+        
+        // Public Variables
+        #endregion
+        
         #endregion
         
         #region Unity API
+
+        private void Awake()
+        {
+            // Find instance of this class, if existent -> destroy that instance
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                Error("There is already an instance of this class! Destroying this one!");
+                return;
+            }
+
+            // Assign instance as this current object
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            _controlPanel = GDControlPanel.Instance;
+            // _sceneLoader = SceneLoader.Instance;
             _currentStationIndex = 0;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            //todo: remove update variable assignment
+            _compressionFactor = _controlPanel.CompressionFactor;
         }
         #endregion
         
@@ -55,20 +88,31 @@ namespace Manager.Runtime
                 Error($"Path not calculated!");
                 return;
             }
-            Info($"Starting journey from {_segments[0].DisplayName} to {_segments[^1].DisplayName}");
+            Info($"Preloading Scene: {trainRoute.EndStation.StationScene}");
+            _sceneLoader = GetSceneLoader();
+            _sceneLoader.PreloadScene(trainRoute.EndStation.StationScene);
+            
+            Info($"Starting journey from {_segments[0].GetStationName()} to {_segments[^1].GetStationName()}");
             StartSegment(_currentStationIndex);
         }
 
         private void StartSegment(int index)
         {
-            Info($"Starting segment {index} of {_segments.Count-1}");
+            InfoInProgress($"Starting segment {index} of {_segments.Count-1}");
             if (index >= _segments.Count-1)
             {
                 EndJourney();
                 return;
             }
             
-            Info($"Segment {_segments[index].DisplayName} -> {_segments[index + 1].DisplayName}");
+            InfoInProgress($"Segment {_segments[index].GetStationName()} -> {_segments[index + 1].GetStationName()}");
+            
+            var realTime = _stationNetwork.GetTravelTime(_segments[index], _segments[index + 1]);
+            Info($"Real time: {realTime}");
+            var uiTime = realTime * _compressionFactor;
+            Info($"UI time: {uiTime}");
+            
+            
             EndSegment();
         }
         
@@ -81,8 +125,23 @@ namespace Manager.Runtime
         private void EndJourney()
         {
             InfoDone($"Journey ended.");
+            _sceneLoader.ActivateScene();
         }
         
+        #endregion
+
+        #region Utils
+
+        private SceneLoader GetSceneLoader()
+        {
+            if (_sceneLoader == null)
+            {
+                _sceneLoader = SceneLoader.Instance;
+                if(_sceneLoader == null) Error("SceneLoader not found!");
+            }
+            return _sceneLoader;
+        }
+
         #endregion
     }
 }
---
+++ UntitledTrainGame/Assets/_/Features/Tools/Runtime/SceneLoader.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using Foundation.Runtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Tools.Runtime
+{
+    public class SceneLoader : FMono
+    {
+        #region Variables
+        
+        #region Private
+        // Private Variables
+        
+        [SerializeField] private string _sceneName;
+        [SerializeField] private string _scenePath = "_/Levels/";
+        private AsyncOperation _loadedScene;
+        private Scene _sceneToUnload;
+        
+        // Private Variables
+        #endregion
+        
+        #region Public
+        // Public Variables
+        
+        public static SceneLoader Instance { get; private set; }
+        
+        // Public Variables
+        #endregion
+        
+        #endregion
+        
+        #region Unity API
+
+        private void Awake()
+        {
+            // Find instance of this class, if existent -> destroy that instance
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                Error("There is already an instance of this class! Destroying this one!");
+                return;
+            }
+
+            // Assign instance as this current object
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        #endregion
+        
+        #region Main Methods
+
+        public void PreloadScene(string sceneName)
+        {
+            _sceneName = sceneName;
+            // _sceneToUnload = SceneManager.GetActiveScene();
+            _sceneToUnload = GetCurrentLevelScene();
+            _loadedScene = SceneManager.LoadSceneAsync($"_{_scenePath}{_sceneName}", LoadSceneMode.Additive);
+            _loadedScene.allowSceneActivation = false;
+        }
+
+        public void ActivateScene()
+        {
+            if (_loadedScene == null)
+            {
+                Error($"No scene loaded!");
+                return;
+            }
+            _loadedScene.allowSceneActivation = true;
+
+            if (_sceneToUnload.IsValid()) StartCoroutine(UnloadPreviousSceneWhenReady());
+            SceneManager.UnloadSceneAsync(_sceneToUnload);
+        }
+        
+        #endregion
+        
+        #region Utils
+
+        private IEnumerator UnloadPreviousSceneWhenReady()
+        {
+            // wait for new scene
+            yield return _loadedScene;
+            
+            // Set new scene as active
+            Scene newScene = SceneManager.GetSceneByName($"{_sceneName}");
+            if (newScene.IsValid())
+            {
+                SceneManager.SetActiveScene(newScene);
+                Info($"Set active scene to: {newScene.name}");
+            }
+            
+            // Unload old scene
+            if (_sceneToUnload.IsValid())
+            {
+                Info($"Unloading previous scene: {_sceneToUnload.name}");
+                AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(_sceneToUnload);
+                yield return unloadOperation;
+                InfoDone($"Scene unloaded: {_sceneToUnload.name}");
+            }
+            
+            _loadedScene = null;
+            _sceneToUnload = default;
+        }
+
+        private Scene GetCurrentLevelScene()
+        {
+            Scene persistentScene = gameObject.scene;
+
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if(scene.IsValid() && scene.isLoaded) return scene;
+            }
+
+            return SceneManager.GetActiveScene();
+        }
+        
+        #endregion
+    }
+}
