af3e85a9cf90f19f60a28d765d82a2e73b4b35cc
AndrewsLgit <andrewsleruth@gmail.com>
2025-09-08 13:25:06 +0200

    Create DialogSystem ScriptableObjects

---
+++ UntitledTrainGame/Assets/_/Features/SharedData/Runtime/ScriptableObjects/Dialog/CharacterData.cs
@@ -0,0 +1,18 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SharedData.Runtime
+{
+    [CreateAssetMenu(fileName = "SO_CharacterData", menuName = "Dialog/CharacterData")]
+    public class CharacterData : ScriptableObject
+    {
+        public string Id;
+        public string Name;
+        [CanBeNull] public Sprite PortraitSprite;
+        [CanBeNull] public Sprite NamePlateSprite;
+        public Sprite DefaultTextBoxSprite;
+        public GameObject UIPrefab;
+        public AudioClip VoiceClip;
+        [CanBeNull] public string[] MetaData;
+    }
+}
